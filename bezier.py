38121c374a12787b468240f1325de5b89273c8b9
jensentsts <jensentsts@163.com>
2024-02-11 23:36:55 +0800

    使用贝塞尔曲线的帕金森（鼠标抽风）模拟器

---
+++ bezier.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/2/11 21:29
+# @Author  : jensentsts
+# @File    : bezier.py
+# @Description : 生成贝塞尔曲线
+
+import pyautogui as pag
+
+
+fact = [1.0]  # 阶乘
+
+
+class Bezier:
+    __point_list: [pag.Point]
+    __point: pag.Point | None
+    __t: float
+    __step_length: float
+    __iter_the_tail: bool  # 迭代到最后一个点
+
+    def __init__(self, points: list[pag.Point], step_length: float = 0.015625):
+        """贝塞尔曲线"""
+        self.__point_list = []
+        self.__point = None  # 当前点的缓存
+        self.__t = 0.0
+        self.__step_length = step_length
+        self.__point_list = points
+        self.__iter_the_tail = False
+        global fact
+        if len(fact) <= len(self.__point_list):
+            for i in range(len(fact), len(self.__point_list) + 1):
+                fact += [fact[i - 1] * i * 1.0]
+
+    def __len__(self):
+        return 1 + 1.0 / self.__step_length
+
+    def __iter__(self):
+        self.__t = 0.0
+        self.__iter_the_tail = False
+        return self
+
+    def __next__(self) -> pag.Point:
+        if self.is_end():
+            if not self.__iter_the_tail:
+                self.__iter_the_tail = True
+                return self.point
+            raise StopIteration()
+        res = self.point
+        self.next()
+        return res
+
+    @property
+    def point_list(self) -> list[pag.Point]:
+        """点列表"""
+        return self.__point_list
+
+    @point_list.setter
+    def point_list(self, value: list | pag.Point):
+        self.__point_list = list(value)
+        self.__t = 0
+        self.__point = self.__point_list[0]
+        # 阶乘数据更新
+        global fact
+        if len(fact) <= len(self.__point_list):
+            for i in range(len(fact), len(self.__point_list) + 1):
+                fact += [fact[i - 1] * i * 1.0]
+
+    def is_end(self) -> bool:
+        return self.__t == 1
+
+    @property
+    def point(self) -> pag.Point:
+        if self.__point is not None:
+            return self.__point
+        global fact
+        # 计算当前的点坐标
+        res = [0.0, 0.0]
+        n = len(self.__point_list) - 1
+        for k, val in enumerate(self.__point_list):
+            para = fact[n] / (fact[k] * fact[n - k]) * (self.__t ** k) * ((1.0 - self.__t) ** (n - k))
+            res[0] += para * val.x
+            res[1] += para * val.y
+        self.__point = pag.Point(res[0], res[1])
+        return self.__point
+
+    @property
+    def t(self) -> float:
+        return self.__t
+
+    @property
+    def step_length(self) -> float:
+        return self.__step_length
+
+    @step_length.setter
+    def step_length(self, value: float):
+        if value > 1.0:
+            raise ValueError
+        if value < 0.0:
+            raise ValueError
+        self.__step_length = value
+
+    @t.setter
+    def t(self, value: float):
+        self.__t = value
+
+    def next(self):
+        """
+        下一个点
+        :return:
+        """
+        self.__t += self.__step_length
+        self.__point = None
+        # 越界判定
+        if self.__t > 1.0:
+            self.__t = 1.0
+        if self.__t < 0.0:
+            self.__t = 0.0
+
+
---
+++ main.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/2/11 21:18
+# @Author  : jensentsts
+# @File    : main.py
+# @Description : 帕金森模拟器main
+
+import pyautogui as pag
+import random
+import time
+import bezier
+
+range_left = -256
+range_right = 256
+
+if __name__ == '__main__':
+    print("How to stop the program:")
+    print("Move your mouse to the left down point of your screen, then it will break down.")
+    screen_size = pag.size()
+    while True:
+        [x, y] = [0, 0]
+        [last_x, last_y] = [0, 0]
+        point_list = []
+        for i in range(0, random.randint(2, 8)):
+            x += random.randint(range_left, range_right)
+            y += random.randint(range_left, range_right)
+            point_list += [pag.Point(x, y)]
+        print(point_list)
+        for i in bezier.Bezier(point_list):
+            mouse_pos = pag.position()
+            move_x = int(i.x - last_x + mouse_pos.x)
+            move_y = int(i.y - last_y + mouse_pos.y)
+            last_x = i.x
+            last_y = i.y
+            if move_x < 0:
+                move_x = 0
+            if move_x > screen_size.width:
+                move_x = screen_size.width - 1
+            if move_y < 0:
+                move_y = 0
+            if move_y > screen_size.height:
+                move_y = screen_size.height - 1
+            pag.moveTo(move_x, move_y)
+
